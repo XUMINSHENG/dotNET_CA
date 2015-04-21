f2930c402bb369781ce5d1cf737e5699bea360ac
XuMinsheng <349768441@qq.com>
2015-04-21 11:34:48 +0800

    Revert "update to V0.21"
    This reverts commit 22ccfb97da1846c065b76f528fc0af4479e87090.

--- CourseRegistration.Data/CourseContext.cs
+++ CourseRegistration.Data/CourseContext.cs
@@ -16,9 +16,6 @@ namespace CourseRegistration.Data
         {
         }
 
-        public DbSet<Staff> Staff { get; set; }
-        public DbSet<SystemAdmin> SystemAdmins { get; set; }
-        public DbSet<CourseAdmin> CourseAdmins { get; set; }
         public DbSet<Instructor> Instructors { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Course> Courses { get; set; }
