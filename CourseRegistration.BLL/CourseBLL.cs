ddc462bf94f0d058e49faa1ef3a0991fc93a7f33
XUMINSHENG <349768441@qq.com>
2015-05-04 16:13:33 +0800

    CourseBLL

--- CourseRegistration.BLL/CourseBLL.cs
+++ CourseRegistration.BLL/CourseBLL.cs
@@ -24,39 +24,66 @@ namespace CourseRegistration.BLL
 
         public void CreateCourse(Course c)
         {
-            uow.CourseRepository.Insert(c);
-            uow.Save();
+            using(IUnitOfWork uow = new UnitOfWork()){
+                uow.CourseRepository.Insert(c);
+                uow.Save();
+            }
         }
 
         public IEnumerable<Course> GetAllCourses()
         {
-            return uow.CourseRepository.GetAll().ToList();
+            using(IUnitOfWork uow = new UnitOfWork()){
+                return uow.CourseRepository.GetAll().ToList();
+            }
         }
 
         public Course GetCourseById(String courseCode)
         {
-            return uow.CourseRepository.GetAll().Where<Course>(x => x.CourseCode == courseCode).Single();
+            using (IUnitOfWork uow = new UnitOfWork())
+            {
+                return uow.CourseRepository.GetAll().Where<Course>(x => x.CourseCode == courseCode).Single();
+            }
+        }
+
+        public void EditCourse(Course c)
+        {
+            using (IUnitOfWork uow = new UnitOfWork())
+            {
+                uow.CourseRepository.Edit(c);
+                uow.Save();
+            }
+        }
+        public void DeleteCourse(Course c)
+        {
+            using (IUnitOfWork uow = new UnitOfWork())
+            {
+                uow.CourseRepository.Delete(c);
+                uow.Save();
+            }
         }
 
         public IEnumerable<Course> SearchCourse(String key)
         {
             IQueryable<Course> result;
             String keyWord = key.ToUpper();
-            //title
-            IQueryable<Course> titleQuery = uow.CourseRepository.GetAll().Where<Course>(x => x.CourseTitle.ToUpper().Contains(keyWord));
-            //category
-            IQueryable<Course> categoryQuery = uow.CourseRepository.GetAll().Where<Course>(x => x.Category.CategoryName.ToUpper().Contains(keyWord));
-            //instructor
-            IQueryable<Course> instructorQuery = from course in uow.CourseRepository.GetAll() 
-                              where
-                                  (from instructor in course.Instructors where instructor.InstructorName.ToUpper().Contains(keyWord) select true).FirstOrDefault() != null
-                              select course;
+            using (IUnitOfWork uow = new UnitOfWork())
+            {
+                //title
+                IQueryable<Course> titleQuery = uow.CourseRepository.GetAll().Where<Course>(x => x.CourseTitle.ToUpper().Contains(keyWord));
+                //category
+                IQueryable<Course> categoryQuery = uow.CourseRepository.GetAll().Where<Course>(x => x.Category.CategoryName.ToUpper().Contains(keyWord));
+                //instructor
+                IQueryable<Course> instructorQuery = from course in uow.CourseRepository.GetAll()
+                                                     where
+                                                         (from instructor in course.Instructors where instructor.InstructorName.ToUpper().Contains(keyWord) select true).FirstOrDefault() != null
+                                                     select course;
 
-            //description
-            IQueryable<Course> descriptionQuery = uow.CourseRepository.GetAll().Where<Course>(x => x.CourseDescription.ToUpper().Contains(keyWord));
+                //description
+                IQueryable<Course> descriptionQuery = uow.CourseRepository.GetAll().Where<Course>(x => x.CourseDescription.ToUpper().Contains(keyWord));
 
-            result = titleQuery.Union(categoryQuery).Union(instructorQuery).Union(descriptionQuery);
-            return result.ToList();
+                result = titleQuery.Union(categoryQuery).Union(instructorQuery).Union(descriptionQuery);
+                return result.ToList();
+            }
         }
 
 
