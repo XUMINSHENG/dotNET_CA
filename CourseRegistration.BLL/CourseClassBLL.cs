d269d8a8f69b9ab854202ed724ccff5f348d72fc
proliuxz <345832454@qq.com>
2015-05-04 15:42:41 +0800

    lxz
    3:42

--- CourseRegistration.BLL/CourseClassBLL.cs
+++ CourseRegistration.BLL/CourseClassBLL.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CourseRegistration.Models;
+using CourseRegistration.Data;
 
 namespace CourseRegistration.BLL
 {
     public class CourseClassBLL
     {
+        private static IUnitOfWork uow = new UnitOfWork();
+        private static readonly Lazy<CourseClassBLL> lazy =
+            new Lazy<CourseClassBLL>(() => new CourseClassBLL());
+
+        public static CourseClassBLL Instance { get { return lazy.Value; } }
+
+        public void CreateCourseClass(CourseClass cc)
+        {
+            uow.CourseClassRepository.Insert(cc);
+            uow.Save();
+        }
+
+        public CourseClass getCourseClassById(int courseClassCode)
+        {
+            return uow.CourseClassRepository.GetAll().Where<CourseClass>(x => x.ClassId == courseClassCode).Single();
+        }
+
+        public IEnumerable<CourseClass> getAllCategories()
+        {
+            return uow.CourseClassRepository.GetAll().ToList();
+        }
+
     }
 }
