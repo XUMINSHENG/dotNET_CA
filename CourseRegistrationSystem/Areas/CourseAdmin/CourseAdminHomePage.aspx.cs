67aef74f106def83a8672f17ec6dd446effd7cb4
proliuxz <A0136010A@CR2123.iss.nus.edu.sg>
2015-05-08 15:44:32 +0800

    lxz
    3:44

--- CourseRegistrationSystem/Areas/CourseAdmin/CourseAdminHomePage.aspx.cs
+++ CourseRegistrationSystem/Areas/CourseAdmin/CourseAdminHomePage.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CourseRegistration.BLL;
 
 
 namespace CourseRegistrationSystem.Areas.CourseAdmin
@@ -12,7 +14,24 @@ namespace CourseRegistrationSystem.Areas.CourseAdmin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Bind();
+        }
 
+        protected void Bind()
+        {
+            GridView1.DataSource = CourseBLL.Instance.GetAllCourses();
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            this.GridView1.PageIndex = e.NewPageIndex;
+            Bind();
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            Bind();
         }
     }
 }
--- CourseRegistrationSystem/Areas/CourseAdmin/CourseAdminHomePage.aspx.designer.cs
+++ CourseRegistrationSystem/Areas/CourseAdmin/CourseAdminHomePage.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace CourseRegistrationSystem.Areas.CourseAdmin {
     
     
     public partial class CourseAdminIndex {
+        
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
     }
 }
