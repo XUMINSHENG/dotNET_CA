d29f116285fc071ea338f5b87430da898f017e4c
proliuxz <345832454@qq.com>
2015-05-27 00:40:07 +0800

    lxz
    Course Admin Menu

--- CourseRegistrationSystem/Areas/CourseAdmin/CourseAdmin.Master.designer.cs
+++ CourseRegistrationSystem/Areas/CourseAdmin/CourseAdmin.Master.designer.cs
@@ -130,6 +130,15 @@ namespace CourseRegistrationSystem.Areas.CourseAdmin {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder Menu;
         
         /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
