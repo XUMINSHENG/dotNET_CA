a80996dcfbaf7b618a46cef81d7873aa823d3a54
xiejiabao <a0109328@u.nus.edu>
2015-05-27 19:56:50 +0800

    XIE

--- CourseRegistrationSystem/Controllers/ClassController.cs
+++ CourseRegistrationSystem/Controllers/ClassController.cs
@@ -27,7 +27,17 @@ namespace CourseRegistrationSystem.Controllers
         public ActionResult RegisterClass(String id)
         {
             CourseClass courseClass = CourseClassBLL.Instance.GetCourseClassById(id);
+            //get personal details to pre fill paticipant details
+
             return View(courseClass);
+        }
+
+        public ActionResult ParticipantView()
+        {
+            //get personal info if indivual user
+            //get employees list if company user
+            Participant p = new Participant();
+            return PartialView(p);
         }
     }
 }
