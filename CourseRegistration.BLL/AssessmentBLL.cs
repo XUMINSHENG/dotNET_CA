5642bc327dce5b2516a515c07f0bb0c56a312d82
XuMinsheng <349768441@qq.com>
2015-06-04 10:36:53 +0800

    create BLL for WCF

---
+++ CourseRegistration.BLL/AssessmentBLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourseRegistration.Models;
+using CourseRegistration.Data;
+
+namespace CourseRegistration.BLL
+{
+    public class AssessmentBLL
+    {
+
+        private static readonly Lazy<AssessmentBLL> lazy =
+            new Lazy<AssessmentBLL>(() => new AssessmentBLL());
+
+        public static AssessmentBLL Instance { get { return lazy.Value; } }
+
+        private AssessmentBLL()
+        {
+
+        }
+
+        public void CreateAssessment(Assessment assessment)
+        {
+            IUnitOfWork uow = UnitOfWorkHelper.GetUnitOfWork();
+            uow.AssessmentRepository.Insert(assessment);
+            uow.Save();
+        }
+    }
+}
---
+++ CourseRegistration.BLL/AttendanceBLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourseRegistration.Models;
+using CourseRegistration.Data;
+
+namespace CourseRegistration.BLL
+{
+    public class AttendanceBLL
+    {
+
+        private static readonly Lazy<AttendanceBLL> lazy =
+            new Lazy<AttendanceBLL>(() => new AttendanceBLL());
+
+        public static AttendanceBLL Instance { get { return lazy.Value; } }
+
+        private AttendanceBLL()
+        {
+
+        }
+
+        public void CreateAttendance(Attendance attendance)
+        {
+            IUnitOfWork uow = UnitOfWorkHelper.GetUnitOfWork();
+            uow.AttendanceRepository.Insert(attendance);
+            uow.Save();
+        }
+    }
+}
